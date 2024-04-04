62840d6cac9ba50100988b4dd7e14a3bb883a4b8
ariaewang <aria.wang@winsor.edu>
2024-04-04 11:31:42 -0400

    Create WinsorApps.cs

---
+++ WinsorApps.cs
@@ -0,0 +1,8 @@
+namespace CS_First_HTTP_Client;
+
+public readonly record struct Login(string email, string password);
+
+public readonly record struct ErrorResponse(string type, string error);
+
+public readonly record struct AuthResponse(string userId, string jwt, DateTime expires, string refreshToken);
+
