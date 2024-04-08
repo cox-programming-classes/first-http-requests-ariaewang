011bf83e8afdd807e78bbc4e797fd9e3493d5223
ariaewang <aria.wang@winsor.edu>
2024-04-08 13:40:24 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using CS_First_HTTP_Client;
 
-string baseAddress = "https://cat-fact.herokuapp.com";
-
-using var client = new HttpClient();
-client.BaseAddress = new(baseAddress);
-
-var request = new HttpRequestMessage(HttpMethod.Get, "facts");
-
-var response = client.Send(request);
-
-if (response.IsSuccessStatusCode)
+HttpClient client = new()
 {
-    var content = (List<CatFact>?)JsonSerializer.Deserialize(
-        response.Content.ReadAsStream(), typeof(List<CatFact>));
+    BaseAddress = new Uri("https://forms-dev.winsor.edu")
+};
 
-    if (content is null)
-    {
-        Console.WriteLine("Something went wrong...");
-        return;
-    }
+#region Build Authentication Request
+var login = new Login("aria.wang@winsor.edu", "fnr843MXU%!&");
 
-    foreach (var fact in content)
-    {
-        Console.WriteLine(fact.text);
-    }
-}
-else
-{
-    Console.WriteLine($"Request failed with status code {response.StatusCode}");
-}
+var jsonContent = JsonSerializer.Serialize(login);
+
+var request = new HttpRequestMessage(HttpMethod.Post, "api/auth");
+request.Content = new StringContent(
+    jsonContent, Encoding.UTF8, "application/json");
+#endregion
+
+var response = await client.SendAsync(request);
+
+var jsonResponse = await response.Content.ReadAsStringAsync();
+
+AuthResponse auth = JsonSerializer.Deserialize<AuthResponse>(jsonResponse);
+
+Console.WriteLine(auth);
+
+request = new(HttpMethod.Get,"api/users/self");
+request.Headers.Authorization =
+    new("Bearer", auth.jwt);
+
+response = await client.SendAsync(request);
+jsonResponse = await response.Content.ReadAsStringAsync();
+UserInfo user = JsonSerializer.Deserialize<UserInfo>(jsonResponse);
+Console.WriteLine(user);
