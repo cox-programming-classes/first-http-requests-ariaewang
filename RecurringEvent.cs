9d166cd21fc96a752e4d12b45ed20a7a95198555
ariaewang <aria.wang@winsor.edu>
2024-04-26 10:52:23 -0400

    Update RecurringEvent.cs

--- RecurringEvent.cs
+++ RecurringEvent.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+
 namespace CS_First_HTTP_Client;
 
 public class RecurringEvent
@@ -5,5 +7,12 @@ public class RecurringEvent
     
     
     public async Task<ImmutableArray<RecurringEvent>> GetRecurringEvent(
+        bool isPublic = true);
+    
+    public async Task<ImmutableArray<RecurringEvent>> GetRecurringEvent(
         bool allDay = false);
+
+    public readonly record struct attendees["aria.wang@winsor.edu", "jinseo.kim@winsor.edu"];
+
+    public readonly record struct cycleDays["Day 1", "Day 6"];
 }
